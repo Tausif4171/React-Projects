ae063b77d6d440137f76d54ed0a3da3046c670db
Tausif4171 <tausifkhan4171@gmail.com>
2021-09-28 23:41:16 +0530

    Updated file

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -2,6 +2,7 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Link } from 'react-router-dom';
 
 
 const ContactCard = (props) => {
@@ -13,19 +14,21 @@ const ContactCard = (props) => {
             <div className="content" style={{ marginTop: 20, lineHeight: 2 }}>
                 <Container style={{ marginLeft: 260 }}>
                     <Row xs={2} md={4} lg={12}>
-                        <Col>
-                            <div className="header">
-                                {name}
-                            </div>
-                        </Col>
-                        <Col>
-                            <div className="header">
-                                {email}
-                            </div>
-                        </Col>
-                        <Col>
+                        <Link to={`/contact/${id}`}>
+                            <Col style={{ marginLeft: 0 }}>
+                                <div className="header">
+                                    {name}
+                                </div>
+                            </Col>
+                            <Col style={{ marginLeft: 0 }}>
+                                <div className="header">
+                                    {email}
+                                </div>
+                            </Col>
+                        </Link>
+                        <Col style={{ marginLeft: 230 }}>
                             <i className="trash alternate outline icon"
-                                style={{ color: "red" }}
+                                style={{ color: "red",fontSize:22 }}
                                 onClick={() => props.clickHandler(id)} ></i>
                         </Col>
                     </Row>
